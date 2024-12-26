0ea83da6ce1f9d366686fdb220a970422ada46e4
vorlannasywan <vorlan.nasywan@mhs.itenas.ac.id>
2024-12-26 21:41:50 +0700

    second commit

--- server.js
+++ server.js
@@ -9,31 +9,27 @@ import TransactionRoutes from './routes/TransactionRoutes.js';
 import cors from 'cors';
 
 
-
-
 dotenv.config();
 const app = express();
 
-// Resolve __dirname
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.use(cors());
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-// Routes
 app.use('/api/users', UserRoutes);
 app.use('/api/games', GameRoutes);
 app.use('/api/transactions', TransactionRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Root Route
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-// Sync Database and Start Server
+
 sequelize.sync().then(() => {
     app.listen(process.env.PORT || 3000, () => {
         console.log(`Server running on port ${process.env.PORT || 5000}`);
